5847c85a2612663f66bc756835f8f0d4c6dbf629
OlenaPavlovska <100314322+OlenaPavlovska@users.noreply.github.com>
2023-09-03 12:34:41 +0300

    fix

--- src/components/ContactAddForm/ContactAddForm.jsx
+++ src/components/ContactAddForm/ContactAddForm.jsx
@@ -86,10 +86,7 @@ export const ContactAddForm = () => {
           margin="none"
           id="name"
           label=""
-          inputProps={{
-            pattern:
-              "^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$",
-          }}
+         
         />
       </Box>
 
@@ -109,7 +106,7 @@ export const ContactAddForm = () => {
           fullWidth
           id="number"
           label="NUMBER"
-          inputProps={{ inputMode: 'numeric', pattern:"\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}" }}
+         
         />
       </Box>
       <Button
